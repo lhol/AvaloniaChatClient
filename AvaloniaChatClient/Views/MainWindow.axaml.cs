97b68d81e8f4a6cca35b29e3d9e5df17cdbeb61a
Lars Holzinger <Lars.Holzinger@gmx.de>
2026-05-10 00:15:55 +0200

    Projektdateien hinzufügen.

---
+++ AvaloniaChatClient/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaChatClient.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
